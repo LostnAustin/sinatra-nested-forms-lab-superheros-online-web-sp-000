c4e66a874bddbe4f72061bcca7d9a10407a54f22
Chad Lengefeld <chadlengefeld@gmail.com>
2020-04-24 17:37:46 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,36 +6,49 @@ class App < Sinatra::Base
 
     get '/' do
 
-        erb :"/team"
+        erb :"/super_hero"
     end
 
-    # get '/team' do
-    #   erb :team
-    # end
+
+
+
 
     post '/teams' do
-      @team = params
+      #  binding.pry
       @team_name = params[:team][:name]
       @motto = params[:team][:motto]
-      @member1_name = params[:team][:hero][0][:name]
-      @member1_power = params[:team][:hero][0][:power]
-      @member1_bio = params[:team][:hero][0][:biography]
+      @hero_name = []
+      @hero_power = []
+      @hero_bio = []
 
-      @member2_name = params[:team][:hero][1][:name]
-      @member2_power = params[:team][:hero][1][:power]
-      @member2_bio = params[:team][:hero][2][:biography]
+      @heroes = params[:team][:hero]
+      @heroes.each do |hero|
 
-      @member3_name = params[:team][:hero][2][:name]
-      @member3_power = params[:team][:hero][2][:power]
-      @member3_bio = params[:team][:hero][2][:biography]
-      #  binding.pry
-      erb :team
+        @hero_name << hero[:name]
+        @hero_power << hero[:power]
+        # binding.pry
+        @hero_bio << hero[:bio]
+
+        erb :"/super_hero"
+# binding.pry
+      end
     end
 
+
+    #   @member1_name = params[:team][:hero][0][:name]
+    #   @member1_power = params[:team][:hero][0][:power]
+    #   @member1_bio = params[:team][:hero][0][:biography]
     #
-    get '/teams' do
+    #   @member2_name = params[:team][:hero][1][:name]
+    #   @member2_power = params[:team][:hero][1][:power]
+    #   @member2_bio = params[:team][:hero][2][:biography]
+    #
+    #   @member3_name = params[:team][:hero][2][:name]
+    #   @member3_power = params[:team][:hero][2][:power]
+    #   @member3_bio = params[:team][:hero][2][:biography]
 
-      erb :team
-    end
 
-end
+
+
+
+ end
